b1d7dcb1e69a3fa682b630ad10e065f823eff3e0
smt <710552832@qq.com>
2026-04-20 22:44:47 +0800

    Initial commit

---
+++ src/main/java/com/smt/LangChain/LLMManager.java
@@ -0,0 +1,19 @@
+package com.smt.LangChain;
+
+import dev.langchain4j.model.openai.OpenAiChatModel;
+import org.apache.log4j.Logger;
+
+public class LLMManager {
+
+    private static final String TAG = "ToolsExecute";
+
+    private static final Logger logger = Logger.getLogger(TAG);
+
+    private OpenAiChatModel model1_5PRO = OpenAiChatModel.builder()
+            .apiKey("d9fca42a-bd84-46fc-898f-4d04eada5ae6")
+            .modelName("ep-20250414124020-qq6d7")
+            .baseUrl("https://ark.cn-beijing.volces.com/api/v3")
+            .build();
+
+
+}
