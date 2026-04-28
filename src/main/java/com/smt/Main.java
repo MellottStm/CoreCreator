b90c6aece6c3af4b3815a680f729e95a0dad8f16
mento_su <710552832@qq.com>
2026-04-28 15:25:05 +0800

    refactor(Main): 更新主应用程序启动逻辑
    - 将 start 方法参数从 primaryStage 重命名为 stage
    - 更改 FXML 加载路径从 DiffView.fxml 到 MainView.fxml
    - 添加控制器获取和阶段设置逻辑
    - 更新窗口标题为 CoreCreator
    - 移除已注释的旧启动方法代码

--- src/main/java/com/smt/Main.java
+++ src/main/java/com/smt/Main.java
@@ -10,29 +10,17 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(Main.class.getResource("/View/DiffView.fxml"));
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource("/View/MainView.fxml"));
         Parent root = loader.load();
-        Scene scene = new Scene(root,1600,900);
-        primaryStage.setTitle("IDEA风格代码编辑器");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        MainController mainController = loader.getController();
+        mainController.setStage(stage);
+        Scene scene = new Scene(root, 1600, 900);
+        stage.setTitle("CoreCreator");
+        stage.setScene(scene);
+        stage.show();
     }
-
-
-//    @Override
-//    public void start(Stage stage) throws Exception {
-//        FXMLLoader loader = new FXMLLoader(Main.class.getResource("/View/MainView.fxml"));
-//        Parent root = loader.load();
-//        MainController mainController = loader.getController();
-//        mainController.setStage(stage);
-//        Scene scene = new Scene(root, 1600, 900);
-//        stage.setTitle("CoreCreator");
-//        stage.setScene(scene);
-//        stage.show();
-//    }
 
     public static void main(String[] args) {
         launch(args);
