5d2481357b3742c82a5a8641e36c54243e0b125b
mento_su <710552832@qq.com>
2026-05-11 15:39:28 +0800

    refactor(TestLLM): 重构测试类的聊天功能实现
    - 添加 UserMessage、ChatResponse 和 StreamingChatResponseHandler 导入
    - 新增 testModel 方法实现流式聊天响应处理
    - 将原来的 main 方法逻辑拆分为独立的测试方法
    - 实现 onCompleteResponse 和 onPartialResponse 回调处理
    - 保留原有的 asyncLangChain 异步处理功能作为 test 方法
    - 使用 CompletableFuture 处理聊天响应的完成状态

--- src/main/java/com/smt/TestLLM.java
+++ src/main/java/com/smt/TestLLM.java
@@ -4,6 +4,9 @@ import com.smt.LangChain.Bean.ContentBean;
 import com.smt.LangChain.LLMManager;
 import com.smt.LangChain.ToolsPrompt;
 import dev.langchain4j.data.message.ChatMessage;
+import dev.langchain4j.data.message.UserMessage;
+import dev.langchain4j.model.chat.response.ChatResponse;
+import dev.langchain4j.model.chat.response.StreamingChatResponseHandler;
 import org.apache.log4j.Logger;
 import java.util.ArrayList;
 import java.util.List;
@@ -19,40 +22,69 @@ public class TestLLM {
 
 
     public static void main(String[] args) {
-       LLMManager llmManager = new LLMManager("F:\\TestProject\\Test");
-       String query = "帮我写一个okhttp的get请求，并且添加依赖到相关文件中";
-       long token = System.currentTimeMillis();
-       llmManager.asyncLangChain(chatMessageList, query,token ,new LLMManager.FluxCallBack() {
-           @Override
-           public CompletableFuture<Void> llmStream(String result,long token) {
-               return CompletableFuture.runAsync(new Runnable() {
-                   @Override
-                   public void run() {
-                       logger.info("中间输出:" + result);
-                   }
-               });
-           }
-
-           @Override
-           public CompletableFuture<Void> finalResult(String result,long token) {
-               return CompletableFuture.runAsync(new Runnable() {
-                   @Override
-                   public void run() {
-
-                   }
-               });
-           }
-
-           @Override
-           public void showDiff(List<ContentBean> list, long token) {
-               for (ContentBean bean : list) {
-                   logger.info("文件:" + bean.path + ",内容:" + bean.content + ",类型:" + bean.operationType);
-               }
-           }
-       });
-
-
-
+        testModel();
     }
+
+
+    public static void testModel () {
+        LLMManager llmManager = new LLMManager("F:\\TestProject\\Test");
+        chatMessageList.add(UserMessage.from("你爸爸是谁？"));
+        CompletableFuture<ChatResponse> completableFuture = new CompletableFuture<>();
+        llmManager.createStreamModel().chat(chatMessageList, new StreamingChatResponseHandler() {
+
+            @Override
+            public void onPartialResponse(String partialResponse) {
+                logger.info(partialResponse);
+            }
+
+            @Override
+            public void onCompleteResponse(ChatResponse chatResponse) {
+                logger.info(chatResponse);
+                completableFuture.complete(chatResponse);
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+
+            }
+        });
+        completableFuture.join();
+    }
+
+
+    public static void test () {
+        LLMManager llmManager = new LLMManager("F:\\TestProject\\Test");
+        String query = "帮我写一个okhttp的get请求，并且添加依赖到相关文件中";
+        long token = System.currentTimeMillis();
+        llmManager.asyncLangChain(chatMessageList, query,token ,new LLMManager.FluxCallBack() {
+            @Override
+            public CompletableFuture<Void> llmStream(String result,long token) {
+                return CompletableFuture.runAsync(new Runnable() {
+                    @Override
+                    public void run() {
+                        logger.info("中间输出:" + result);
+                    }
+                });
+            }
+
+            @Override
+            public CompletableFuture<Void> finalResult(String result,long token) {
+                return CompletableFuture.runAsync(new Runnable() {
+                    @Override
+                    public void run() {
+
+                    }
+                });
+            }
+
+            @Override
+            public void showDiff(List<ContentBean> list, long token) {
+                for (ContentBean bean : list) {
+                    logger.info("文件:" + bean.path + ",内容:" + bean.content + ",类型:" + bean.operationType);
+                }
+            }
+        });
+    }
+
 
 }
